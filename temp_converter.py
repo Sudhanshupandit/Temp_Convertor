b5685a33fe6267467b7c30b4b6780a15d5e9697a
Sudhanshupandit <84499696+Sudhanshupandit@users.noreply.github.com>
2023-11-14 21:49:12 +0530

    Add files via upload

---
+++ temp_converter.py
@@ -0,0 +1,28 @@
+celsius_1 = float(input("Temperature value in degree Celsius: " ))    
+Fahrenheit_1 = (celsius_1 * 1.8) + 32    
+print('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_1, Fahrenheit_1))
+
+print("----OR----")  
+celsius_2 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_2 = (celsius_2 * 9/5) + 32    
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_2, Fahrenheit_2))
+
+print("----OR----")
+celsius_3 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_3 = (celsius_3 * 9/5) + 32    
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_3, Fahrenheit_3))
+
+print("----OR----")
+celsius_4 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_4 = (celsius_4 * 9/5) + 32    
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_4, Fahrenheit_4))
+
+print("----OR----")
+celsius_5 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_5 = (celsius_5 * 9/5) + 32    
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
+      %(celsius_5, Fahrenheit_5))  
